557f6ba922c05d9ec1246243a25d64566480f07c
sham00n <51245785+sham00n@users.noreply.github.com>
2019-08-03 00:01:36 +0200

    Update aboutme.py

--- buster/lib/modules/aboutme.py
+++ buster/lib/modules/aboutme.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 #checks if an email has an about.me acccount associated with it and returns a link to the profile
 def email2aboutme(email):
 
-	url="https://about.me/n/password/find_account"
+	
 
 	session = requests.Session()
 
@@ -17,26 +17,23 @@ def email2aboutme(email):
 		AUTH_TOKEN = json.loads( soup.find('script','contextData').text )['globals']['AUTH_TOKEN']
 
 
-		response=session.post(url,data={'email_address': email},headers={'X-Auth-Token':AUTH_TOKEN}).json()
+		response=session.post("https://about.me/n/password/find_account",data={'email_address': email},headers={'X-Auth-Token':AUTH_TOKEN}).json()
 
 	
 		if 'user_name' in response:
-			return "https://about.me/" + response['user_name']
+			accounts=["https://about.me/" + response['user_name']]
+			response=requests.get(accounts[0])
+			soup = BeautifulSoup(response.content, 'html.parser')
+			for link in soup.find_all('a',class_='social-link'):
+				accounts.append(link.get('href'))
+			
+			return accounts
 		else:
-			return ""
+			return []
 	
 	except:
 		print("[=]Warning:Something went wrong while attempting to scrap about.me")
-		return ""
-
-
-
-
-
-
-
-
-
+		return []
 
 
 
