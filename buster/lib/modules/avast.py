db8c65503b11ccdd67ad8e652fafbb5fe44870cf
sham00n <51245785+sham00n@users.noreply.github.com>
2019-08-03 00:02:14 +0200

    Update avast.py

--- buster/lib/modules/avast.py
+++ buster/lib/modules/avast.py
@@ -1,14 +1,22 @@
+import pkg_resources
+import json
 
 def email2breachedaccts(email,response):
 
 	accounts=[]
 	breaches=[]
 
+	with open(pkg_resources.resource_filename('data', 'domain_list.json'),'r') as json_file:
+		domains = json.loads(json_file.read())
+
 	if response.status_code==200:
 		data=response.json()["value"]
 		for breach in data:
 			if breach["domain"] != "" and breach["username"] != email :
-				accounts.append(breach["domain"] + " : " + breach["username"])
+				if breach["domain"] in domains:
+					accounts.append(domains[breach["domain"]].replace('{}',breach["username"]))
+				else:
+					accounts.append(breach["domain"] + " : " + breach["username"])
 			if breach["domain"] != "":
 				breaches.append(breach["domain"])
 				
@@ -16,5 +24,4 @@ def email2breachedaccts(email,response):
 
 
 	return {'accounts':accounts,'breaches':breaches}
-
 
