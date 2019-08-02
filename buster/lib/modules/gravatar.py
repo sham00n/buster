1251dbbcb81d1c4eb70e9104aa7411aff30ff766
sham00n <51245785+sham00n@users.noreply.github.com>
2019-08-03 00:02:44 +0200

    Update gravatar.py

--- buster/lib/modules/gravatar.py
+++ buster/lib/modules/gravatar.py
@@ -1,20 +1,25 @@
 import hashlib
 import requests
+import json
 
 #checks if an email has a gravatar acccount associated with it and returns a link to the profile
 def email2gravatar(email):
 	email_hash=hashlib.md5(email.encode('utf-8')).hexdigest()
 	
-	
+	accounts=[]
 	
 	try:
 		response=requests.get("https://www.gravatar.com/" + email_hash)
+		
 		if response.status_code==200:
-			return response.url
-		else:
-			return ""
+			accounts.append(response.url)
+			response=requests.get(response.url + '.json')
+			urls=response.json()['entry'][0]['urls']
+			for url in urls:
+				accounts.append(url['value'])
+
 	except:
 		print("[=]Warning:Something went wrong while attempting to scrap gravatar.com")
-		return ""
-	
+
+	return accounts	
 
